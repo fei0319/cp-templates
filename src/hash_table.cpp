1c252075b8ba7c560d302e491b0618561c6e6aa1
Fei Pan <pan-fei@outlook.com>
2023-11-30 11:18:29 +0800

    Added a test cpp

---
+++ src/hash_table.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+template <typename K, typename V, typename Hasher>
+class HashTable {
+    std::vector<std::vector<std::pair<K, V>>> d;
+public:
+    void resize(std::size_t size) {
+        d.resize(size);
+    }
+    V &operator[](const K &key) {
+        auto &vec = d[Hasher()(key) % d.size()];
+        for (auto &[k, v] : vec) {
+            if (key == k) {
+                return v;
+            }
+        }
+
+        vec.emplace_back(key, V());
+        return vec.back().second;
+    }
+};
