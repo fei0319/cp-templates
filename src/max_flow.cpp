e4a169a3cb1dccc3b9d32b69c779c91a83de2a90
fei0319 <fei0319@users.noreply.github.com>
2023-12-04 08:38:01 +0000

    Applied clang-format

--- src/max_flow.cpp
+++ src/max_flow.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <functional>
-#include <queue>
 #include <algorithm>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <vector>
 
 template <typename T>
 struct Inf {
-	static T value;
+    static T value;
 };
 
 template <>
@@ -14,90 +14,94 @@ int Inf<int>::value = 1e9;
 template <>
 long long Inf<long long>::value = 1e18;
 
-template<typename T>
+template <typename T>
 class Flow {
 private:
-	int s, t, cnt = 1;
-	struct edge {
-		int next, to;
-		T flow;
-	};
-	int n;
-	
-	std::vector <T> hd;
-	std::vector <edge> e;
+    int s, t, cnt = 1;
+    struct edge {
+        int next, to;
+        T flow;
+    };
+    int n;
 
-	void Add (int u, int v, T w) {
-		e[++cnt] = (edge) {hd[u], v, w};
-		hd[u] = cnt;
-	}
-	#define FOR(u) for (int i = hd[u], v; v = e[i]. to, i; i = e[i].next)
-	
-	T query() {
-		
-		std::vector <int> dep(n + 1);
-		
-		std::function <bool()> bfs = [&] () {
-			std::queue <int> q;
-			std::fill (dep.begin(), dep.end(), 0);
-			dep[s] = 1;
-			q.push(s);
-			while (!q.empty()) {
-				int u = q.front();
-				q.pop();
-				FOR(u) {
-					if (!e[i].flow || dep[v]) {
-						continue;
-					}
-					dep[v] = dep[u] + 1;
-					q.push(v);
-				}
-			}
-			return dep[t] != 0 ? 1 : 0;
-		};
-		
-		std::function <T(int, T)> dfs = [&] (int u, T flow) {
-		    if (u == t) return flow;
-		    T res = 0;
-		    FOR(u) {
-		        if (dep[v] != dep[u] + 1 || !e[i].flow)
-		            continue;
-		        T k = dfs (v, std::min (flow - res, e[i].flow));
-		        e[i].flow -= k;
-		        e[i ^ 1].flow += k;
-		        res += k;
-		        if (res == flow) 
-		        	return flow;
-		    }
-		    if (res == 0) dep[u] = 0;
-		    return res;
-		};
-		
-		T ans, maxf = 0;
-	    while (bfs())
-	        while ((ans = dfs(s, Inf<T>::value))) maxf += ans;
-	    return maxf;
-	}
+    std::vector<T> hd;
+    std::vector<edge> e;
+
+    void Add(int u, int v, T w) {
+        e[++cnt] = (edge){hd[u], v, w};
+        hd[u] = cnt;
+    }
+#define FOR(u) for (int i = hd[u], v; v = e[i].to, i; i = e[i].next)
+
+    T query() {
+
+        std::vector<int> dep(n + 1);
+
+        std::function<bool()> bfs = [&]() {
+            std::queue<int> q;
+            std::fill(dep.begin(), dep.end(), 0);
+            dep[s] = 1;
+            q.push(s);
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                FOR(u) {
+                    if (!e[i].flow || dep[v]) {
+                        continue;
+                    }
+                    dep[v] = dep[u] + 1;
+                    q.push(v);
+                }
+            }
+            return dep[t] != 0 ? 1 : 0;
+        };
+
+        std::function<T(int, T)> dfs = [&](int u, T flow) {
+            if (u == t)
+                return flow;
+            T res = 0;
+            FOR(u) {
+                if (dep[v] != dep[u] + 1 || !e[i].flow)
+                    continue;
+                T k = dfs(v, std::min(flow - res, e[i].flow));
+                e[i].flow -= k;
+                e[i ^ 1].flow += k;
+                res += k;
+                if (res == flow)
+                    return flow;
+            }
+            if (res == 0)
+                dep[u] = 0;
+            return res;
+        };
+
+        T ans, maxf = 0;
+        while (bfs())
+            while ((ans = dfs(s, Inf<T>::value)))
+                maxf += ans;
+        return maxf;
+    }
+
 public:
-	Flow (int _n, int m, int _s, int _t) : n(_n), hd(_n + 1), e(m + 1), s(_s), t(_t){};
-	void add (int u, int v, T w) {
-		Add (u, v, w);
-		Add (v, u, 0);
-	}
-	T max_flow() {
-		return query();
-	}
+    Flow(int _n, int m, int _s, int _t)
+        : n(_n), hd(_n + 1), e(m + 1), s(_s), t(_t){};
+    void add(int u, int v, T w) {
+        Add(u, v, w);
+        Add(v, u, 0);
+    }
+    T max_flow() {
+        return query();
+    }
 };
 
-
 int main() {
-	int n, m, s, t;
-	std :: cin >> n >> m >> s >> t;
-	Flow <long long> f(n + 1, m * 2 + 1, s, t); 
-	for (int i = 1, u, v, w; i <= m; ++i) {
-		std::cin >> u >> v >> w;
-		f.add(u, v, w);
-	}
-	std::cout<<f.max_flow()<<'\n';
-	return 0;
-} 
+    int n, m, s, t;
+    std ::cin >> n >> m >> s >> t;
+    Flow<long long> f(n + 1, m * 2 + 1, s, t);
+    for (int i = 1, u, v, w; i <= m; ++i) {
+        std::cin >> u >> v >> w;
+        f.add(u, v, w);
+    }
+    std::cout << f.max_flow() << '\n';
+    return 0;
+}
--- src/min_cost.cpp
+++ src/min_cost.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <functional>
-#include <queue>
 #include <algorithm>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <vector>
 
 template <typename T>
 struct Inf {
-	static T value;
+    static T value;
 };
 
 template <>
@@ -14,94 +14,96 @@ int Inf<int>::value = 1e9;
 template <>
 long long Inf<long long>::value = 1e18;
 
-template<typename T>
+template <typename T>
 class Flow {
 private:
-	int s, t, cnt = 1;
-	struct edge {
-		int next, to;
-		T flow, cost;
-	};
-	int n;
-	
-	std::vector <T> hd;
-	std::vector <edge> e;
+    int s, t, cnt = 1;
+    struct edge {
+        int next, to;
+        T flow, cost;
+    };
+    int n;
 
-	void Add (int u, int v, T w, T c) {
-		e[++cnt] = (edge) {hd[u], v, w, c};
-		hd[u] = cnt;
-	}
-	#define FOR(u) for (int i = hd[u], v; v = e[i]. to, i; i = e[i].next)
-	
-	std::pair <T, T> ek() {
-		std::vector <bool> vis(n + 1);
-		std::vector <T> dis(n + 1), f(n + 1);
-		std::vector <int> pre(n + 1);
-		
-		auto spfa = [&]() {
-		    for (int i = 1; i <= t; ++i)
-		        vis[i] = 0, dis[i] = Inf<T>::value;
-		    std::queue <int> q;
-			q.push(s);
-		    vis[s] = 1;
-			dis[s] = 0;
-			pre[t] = -1;
-			f[s] = Inf<T>::value;
-			while (!q.empty()) {
-		        int u = q.front();
-		        q.pop();
-				vis[u]=0;
-		        FOR(u) {
-		            if (!e[i].flow) continue;
-		            if (dis[v] > dis[u] + e[i].cost) {
-		                dis[v] = dis[u] + e[i].cost;
-		                f[v] = std::min (f[u], e[i].flow);
-		                pre[v] = i;
-		                if (!vis[v]) 
-							vis[v] = 1, q.push(v); 
-		            }
-		        }
-		    }
-		    return pre[t] == -1 ? -1 : f[t];
-		};
-		int maxf, minc;
-	    int u,in,i;
-		maxf=0,minc=0;
-	    while ((in = spfa()) != -1) {
-	        u=t;
-	        while (u != s) {
-	            i = pre[u];
-	            e[i].flow -= in;
-	            e[i^1].flow += in;
-	            u = e[i^1].to;
-	        }
-	        maxf += in;
-	        minc += in*dis[t]; 
-	    }
-	    return std::make_pair (maxf, minc);
-	}
+    std::vector<T> hd;
+    std::vector<edge> e;
+
+    void Add(int u, int v, T w, T c) {
+        e[++cnt] = (edge){hd[u], v, w, c};
+        hd[u] = cnt;
+    }
+#define FOR(u) for (int i = hd[u], v; v = e[i].to, i; i = e[i].next)
+
+    std::pair<T, T> ek() {
+        std::vector<bool> vis(n + 1);
+        std::vector<T> dis(n + 1), f(n + 1);
+        std::vector<int> pre(n + 1);
+
+        auto spfa = [&]() {
+            for (int i = 1; i <= t; ++i)
+                vis[i] = 0, dis[i] = Inf<T>::value;
+            std::queue<int> q;
+            q.push(s);
+            vis[s] = 1;
+            dis[s] = 0;
+            pre[t] = -1;
+            f[s] = Inf<T>::value;
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                vis[u] = 0;
+                FOR(u) {
+                    if (!e[i].flow)
+                        continue;
+                    if (dis[v] > dis[u] + e[i].cost) {
+                        dis[v] = dis[u] + e[i].cost;
+                        f[v] = std::min(f[u], e[i].flow);
+                        pre[v] = i;
+                        if (!vis[v])
+                            vis[v] = 1, q.push(v);
+                    }
+                }
+            }
+            return pre[t] == -1 ? -1 : f[t];
+        };
+        int maxf, minc;
+        int u, in, i;
+        maxf = 0, minc = 0;
+        while ((in = spfa()) != -1) {
+            u = t;
+            while (u != s) {
+                i = pre[u];
+                e[i].flow -= in;
+                e[i ^ 1].flow += in;
+                u = e[i ^ 1].to;
+            }
+            maxf += in;
+            minc += in * dis[t];
+        }
+        return std::make_pair(maxf, minc);
+    }
+
 public:
-	Flow (int _n, int m, int _s, int _t) : n(_n), hd(_n + 1), e(m + 1), s(_s), t(_t){};
-	void add (int u, int v, T w, T c) {
-		Add (u, v, w, c);
-		Add (v, u, 0LL, -c);
-	}
-	void mincost() {
-		std::pair <T, T> x = ek();
-		std::cout << x.first << " " << x.second <<'\n';
-	}
+    Flow(int _n, int m, int _s, int _t)
+        : n(_n), hd(_n + 1), e(m + 1), s(_s), t(_t){};
+    void add(int u, int v, T w, T c) {
+        Add(u, v, w, c);
+        Add(v, u, 0LL, -c);
+    }
+    void mincost() {
+        std::pair<T, T> x = ek();
+        std::cout << x.first << " " << x.second << '\n';
+    }
 };
 
-
 int main() {
-	int n, m;
-	std::cin >> n >> m;
-	Flow <long long> fl(n, m * 2 + 1, 1, n); 
-	for (int i = 1; i <= m; ++i) {
-		int u, v, f, c;
-		std::cin >> u >> v >> f >> c;
-		fl.add(u, v, f, c);
-	}
-	fl.mincost();
-	return 0;
-} 
+    int n, m;
+    std::cin >> n >> m;
+    Flow<long long> fl(n, m * 2 + 1, 1, n);
+    for (int i = 1; i <= m; ++i) {
+        int u, v, f, c;
+        std::cin >> u >> v >> f >> c;
+        fl.add(u, v, f, c);
+    }
+    fl.mincost();
+    return 0;
+}
