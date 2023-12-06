bc216a8ffd2be78a89640fec9958e3f3ff2e6330
fei0319 <fei0319@users.noreply.github.com>
2023-12-06 10:34:12 +0000

    Applied clang-format

--- src/treap.cpp
+++ src/treap.cpp
@@ -3,7 +3,8 @@ class Treap {
         tr[x].size = tr[tr[x].ls].size + tr[tr[x].rs].size + tr[x].cnt;
     }
     std::pair<int, int> split(int x, const int &key) {
-        if (!x) return {0, 0};
+        if (!x)
+            return {0, 0};
         if (tr[x].key >= key) {
             auto [a, b] = split(tr[x].ls, key);
             tr[x].ls = b, push_up(x);
@@ -15,7 +16,8 @@ class Treap {
         }
     }
     int merge(int x, int y) {
-        if (!x || !y) return x + y;
+        if (!x || !y)
+            return x + y;
         if (tr[x].priority > tr[y].priority) {
             tr[x].rs = merge(tr[x].rs, y), push_up(x);
             return x;
@@ -24,13 +26,15 @@ class Treap {
             return y;
         }
     }
-    int rt {0};
+    int rt{0};
+
 public:
     void insert(const int &key) {
         auto [a, b] = split(rt, key);
         auto [c, d] = split(b, key + 1);
         // a c d
-        if (!c) c = new_node(key);
+        if (!c)
+            c = new_node(key);
         tr[c].cnt += 1, tr[c].size += 1;
 
         rt = merge(a, merge(c, d));
@@ -41,7 +45,8 @@ public:
 
         if (c) {
             tr[c].cnt -= 1, tr[c].size -= 1;
-            if (!tr[c].cnt) c = 0;
+            if (!tr[c].cnt)
+                c = 0;
         }
 
         rt = merge(a, merge(c, d));
